f7a6577539abb30ce926d93f2c356d0b21260131
varun-parthasarathy <varunparthasarathy7@gmail.com>
2024-01-28 11:31:23 +0530

    Add Noisy ViT and Linear Transform Noise layer implementations

---
+++ linear_transform_noise_layer.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn.functional as F
+
+
+class LinearTransformNoiseLayer(torch.nn.Module):
+    def __init__(self, alpha=None, metric='L2', simple=False, **kwargs):
+        '''
+        Arguments -
+            alpha  - Sets the linear transform strength. If None, the optimal quality matrix will be computed
+            metric - Sets the distance metric to be used for generating negative pairs for each input using a
+                     heuristic approach. Possible values are ['L1', 'L2', 'cosine'] or int. If an integer is
+                     passed, the corresponding p-norm will be computed.
+            simple - Use simplified method of getting a negative pair - instead of randomly sampling from the 
+                     top n largest distances, take the largest distance only. Can help speed up computations
+        '''
+        super().__init__(**kwargs)
+        self.alpha = alpha
+        if metric == 'L2':
+            self.norm = 2
+        elif metric == 'L1':
+            self.norm = 1
+        else:
+            self.norm = metric if isinstance(metric, int) or metric == 'cosine' else 2
+        self.simple = simple
+
+    def compute_quality_matrix(self, k):
+        if self.alpha is None:
+            Q = (torch.diag(torch.ones(k)) * -k/(k+1)) + (torch.ones(k, k) / (k+1))
+        else:
+            I = torch.diag(torch.ones(k))
+            fI = torch.zeros(k, k)
+            for i in range(k):
+                fI[(i+1)%k, i] = 1 # since f(I) is a cyclic row shift operation
+            Q = (-self.alpha * I) + (self.alpha * fI)
+        return Q
+    
+    def generate_random_sample(self, x):
+        '''
+        Shuffle the batch X to pair each image with one from a different class using the distance between latent space
+        representations. The assumption is that for a classification task, the latent space representation (especially
+        in deeper layers) for images of the same class would be close to each other compared to representations of images
+        from different classes. Note that for this to work effectively, each batch must 
+            a. be as large as possible (as more data in the batch reduces task entropy to a greater extent)
+            b. have a good mix of images from different classes
+        If two images of the same class have a large distance, one of them could be getting mis-classified and thus adding
+        features from that image could force the classifier to learn even better features than before, which could still
+        improve performance. This is not guaranteed, which is why this approach is a heuristic.
+
+        The benefit of this heuristic is that labels are not required in the forward pass. Thus this layer can be dropped
+        into any model with minimal code changes.
+        '''
+        if len(x.shape) > 2:
+            X2 = torch.flatten(x, 1)
+        else:
+            X2 = x
+        if self.norm == 'cosine':
+            dists = 1. - (F.normalize(X2) @ F.normalize(X2).t())
+        else:
+            dists = torch.cdist(F.normalize(X2, p=self.norm), F.normalize(X2, p=self.norm), p=self.norm)
+
+        if self.simple is False:
+            k = max(min(x.shape[0], 8), x.shape[0] // 4) # Arbitrary value for top-k
+            _, largest_k_indices = torch.topk(dists, k)
+            rand_idxs = largest_k_indices[range(x.shape[0]), [torch.randperm(k)[0] for i in range(x.shape[0])]]
+            rand_x = x[rand_idxs]
+            return rand_x
+        else:
+            largest_indices = torch.argmax(dists, dim=1)
+            rand_x = x[largest_indices]
+            return rand_x
+        
+    def forward(self, x):
+        if self.training:
+            # Add noise during training only
+            input_shape = x.shape
+            # Flatten x for matrix multiplication; axes are just abstracts anyway
+            x = torch.flatten(x, 1)
+            Q = self.compute_quality_matrix(input_shape[0])
+            X2 = self.generate_random_sample(x)
+            # Add the linear transform noise
+            x = x + Q@X2
+            x = x.reshape(input_shape)
+            return x
+        else:
+            # During inference, operate as an identity layer
+            return x
+        
+
+if __name__ == '__main__':
+    noise_layer = LinearTransformNoiseLayer()
+    inputs = torch.rand((16, 64, 4, 4))
+    outputs = noise_layer(inputs)
+    print(outputs.shape, outputs.requires_grad)
+
+    # Grad flow check
+    test_model = torch.nn.Sequential(torch.nn.Linear(512, 256), noise_layer, torch.nn.Linear(256, 128), torch.nn.Linear(128, 8))
+    inputs = torch.rand((16, 512))
+    outputs = test_model(inputs)
+    outputs.sum().backward()
+    print(test_model[0].weight.grad, test_model[2].weight.grad, test_model[3].weight.grad)
+
+    # Test eval mode
+    noise_layer.eval()
+    outputs = noise_layer(inputs)
+    print(torch.all(outputs == inputs))
---
+++ noisy_vit.py
@@ -0,0 +1,88 @@
+import torch
+import numpy as np
+from timm.models.vision_transformer import VisionTransformer
+
+def quality_matrix(k, alpha=None):
+    if alpha is None:
+        Q = (torch.diag(torch.ones(k)) * -k/(k+1)) + (torch.ones(k, k) / (k+1))
+    else:
+        I = torch.diag(torch.ones(k))
+        fI = torch.zeros(k, k)
+        for i in range(k):
+            fI[(i+1)%k, i] = 1
+        Q = (-alpha * I) + (alpha * fI)
+    return Q
+
+class NoisyViT(VisionTransformer):
+    def __init__(self, alpha=None, **kwargs):
+        self.alpha = alpha
+        super().__init__(**kwargs)
+
+    @torch.no_grad()
+    def generate_linear_transform_noise(self, x, y, B):
+        # @TODO: Check if the distance between embeddings can be used to generate the shuffled X data?
+        y = torch.squeeze(y) if isinstance(y, torch.Tensor) else torch.squeeze(torch.tensor(y))
+        noise_idxs = []
+        for c in range(B):
+            cur_y = y[c]
+            neg_idxs = torch.where(y != cur_y, True, False).nonzero(as_tuple=True)[0]
+            neg_id = np.random.choice(neg_idxs)
+            noise_idxs.append(neg_id)
+        rand_x = x.clone().detach() # This is noise; we do not want to have this in the graph
+        return rand_x[noise_idxs]
+    
+    def forward_features(self, inp):
+        if isinstance(inp, tuple):
+            x, y = inp
+        else:
+            x = inp
+        if self.grad_checkpointing and not torch.jit.is_scripting():
+            return super().forward_features(x)
+        x = self.patch_embed(x)
+        x = self._pos_embed(x)
+        x = self.patch_drop(x)
+        x = self.norm_pre(x)
+
+        if self.training:
+            # Add noise only in the training phase
+            x = self.blocks[:-1](x)
+            # Assume that the first embedding is the token. We do not want to take the token into account
+            # when adding noise
+            token = x[:, 0, :].unsqueeze(1)
+            x = x[:, 1:, :]
+            B, L, C = x.shape
+            # Flatten for matmul purposes, axes are just abstracts anyway
+            x = torch.flatten(x, 1)
+
+            # The dimensions of the quality matrix are batch_size x batch_size as described in the paper. This is
+            # because the noise itself is the result of linear transformations on the input at the image level
+            with torch.no_grad(): # For safety
+                Q = quality_matrix(B, self.alpha)
+                # Since image X1 needs to receive noise from a different image X2, we sample the negative pairs using labels y
+                # The benefit is that we do not need to perform any separate sampling strategy from the dataset, which allows
+                # this to be dropped into any backbone in theory. The issue however is that labels need to be passed in the
+                # forward pass during training.
+                rand_x = self.generate_linear_transform_noise(x, y, B)
+                # Compute X1 + QX2
+                x = x + Q@rand_x
+            
+            x = x.reshape(B, L, C)
+            x = torch.cat([token, x], dim=1)
+            x = self.blocks[-1](x)
+        else:
+            x = self.blocks(x)
+        x = self.norm(x)
+        return x
+    
+
+# Sanity check
+if __name__ == '__main__':
+    model = NoisyViT(alpha=None,
+                     patch_size=16,
+                     embed_dim=64,
+                     depth=12,
+                     num_heads=4)
+    inputs = torch.rand((16, 3, 224, 224))
+    labels = [82, 82, 3, 2, 4, 1, 1, 5, 6, 7, 8, 15, 17, 241, 99, 62]
+    output = model((inputs, labels))
+    print(output.shape)
